cf0dd3911d49dcbfcd2ad959e966d96cbf1f03d4
takeru0922 <moriai0922takeru@gmail.com>
2020-03-19 21:30:10 +0900

    [finish]3-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,18 +1,30 @@
 class TodolistsController < ApplicationController
 
     def new
-         @list = List.new
+        @list = List.new
     end
-
 
     def create
-
-         list = List.new(list_params)
-
-         list.save
-
+        list = List.new(list_params)
+        list.save
         redirect_to '/top'
     end
+
+    def index
+        @lists = List.all
+    end
+
+    def show
+        @list = List.find(params[:id])
+    end
+
+    def create
+        list = List.new(list_params)
+        list.save
+
+        redirect_to todolist_path(list.id)
+    end
+
     private
 
     def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
-  get 'todolists/new'
+	
+	get 'todolists/new'
+	get 'todolists/:id' => 'todolists#show', as: 'todolist'
 	get 'top'=> 'homes#top'
-
 	post 'todolists' => 'todolists#create'
+	get 'todolists' => 'todolists#index'
 
 end
